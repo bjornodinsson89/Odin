0868ed0d095a1d42f03b2a29514ecd1eeb5e120d
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-09 15:15:59 -0700

    Create Odin.meta.js
    Initial upload

---
+++ main/Odin.meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Odin Tools
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Faction Tools
+// @author       BjornOdinsson89
+// @match        https://www.torn.com/*
+// @match        https://www2.torn.com/*
+// @updateURL    https://raw.githubusercontent.com/BjornOdinsson89/Odin/main/Odin.meta.js
+// @downloadURL  https://raw.githubusercontent.com/BjornOdinsson89/Odin/main/Odin.user.js
+// @grant        GM_notification
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addElement
+// @connect      github.com
+// @connect      raw.githubusercontent.com
+// @connect      api.torn.com
+// @connect      worldtimeapi.org
+// ==/UserScript==
