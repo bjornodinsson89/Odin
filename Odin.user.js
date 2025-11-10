ff69263da6819655486f4cad6dda592165523b93
BjornOdinsson89 <238361375+bjornodinsson89@users.noreply.github.com>
2025-11-09 18:30:54 -0700

    Update Odin.user.js

--- Odin.user.js
+++ Odin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Odin Tools
 // @namespace    http://tampermonkey.net/
-// @version      2.0.1
+// @version      2.1
 // @description  Faction Tools
 // @author       BjornOdinsson89
 // @match        https://www.torn.com/*
